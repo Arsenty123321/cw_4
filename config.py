9a8160e072bad41ddaf323013d40d2a6a69411bc
Arsenty Dafintsev <aaapython24@gmail.com>
2024-07-07 23:53:40 +0300

    Add a selection of pages in the API request

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 HH_API_URL = "https://api.hh.ru/vacancies"
+HH_API_MAX_PAGES = 20
--- src/api.py
+++ src/api.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 import requests
 
-from config import HH_API_URL
+from config import HH_API_URL, HH_API_MAX_PAGES
 
 
 class API(ABC):
@@ -22,23 +23,31 @@ class HHAPI(API):
 
     def __init__(self):
         self.url = HH_API_URL
+        self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {
             'text': '',
-            "per_page": 100,
-            "search_field": "name",
+            'page': 0,
+            'per_page': 100,
+            'search_field': 'name',
+            "area": 113,
         }
+        self.vacancies = []
 
-    def get_vacancies(self, vacancy_name) -> dict:
+    def get_vacancies(self, vacancy_name) -> List[dict]:
         """
         Получение списка вакансий
         """
         self.params["text"] = vacancy_name
 
-        response = requests.get(self.url, params=self.params)
-        if response.status_code != 200:
-            raise ValueError(f'Ошибка запроса данных: status_code={response.status_code} url={self.url}')
-        else:
-            return response.json()['items']
+        while self.params.get('page') != HH_API_MAX_PAGES:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            if response.status_code != 200:
+                raise ValueError(f'Ошибка запроса данных: status_code={response.status_code} url={self.url}')
+            else:
+                vacancies = response.json()['items']
+                self.vacancies.extend(vacancies)
+                self.params['page'] += 1
+            return self.vacancies
 
 
 if __name__ == '__main__':
