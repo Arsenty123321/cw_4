1e64670838507cafa2d82c528a25044942f1f118
Arsenty Dafintsev <aaapython24@gmail.com>
2024-07-08 23:02:39 +0300

    Add vacancy.py

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,47 +2,66 @@ class FromVacancy:
     """
     Класс для работы с вакансиями.
     """
-    __slots__ = ['__name', '__salary_from', '__url', '__requirement']
+    __slots__ = ['__name', '__salary', '__url', '__area_name', '__requirement']
 
-    def __init__(self, name: str, salary_from: int, url: str, requirement: str):
+    def __init__(self, name: str, salary: dict, url: str, area_name: str, requirement: str):
         self.__name = name
-        self.__salary_from = self.__validate_salary(salary_from)
+        self.__salary = self.__validate_salary(salary)
         self.__url = url
-        self.__requirement = requirement
+        self.__area_name = self.__validate_data(area_name)
+        self.__requirement = self.__validate_data(requirement)
 
     @property
     def salary_from(self):
-        return self.__salary_from
+        return self.__salary
 
     def __str__(self):
         return (
             f"Название: {self.__name}\n"
-            f"Зарплата: от {self.__salary_from}\n"
+            f"Зарплата: от {self.__salary}\n"
+            f"Город: {self.__area_name}\n"
             f"Ссылка: {self.__url}\n"
             f"Требования: {self.__requirement}\n"
         )
 
     def __repr__(self):
-        return f'FromVacancy("{self.__name}", {self.__salary_from}, "{self.__url}", "{self.__requirement}")'
+        return (f'FromVacancy("{self.__name}", {self.__salary}, '
+                f'"{self.__url}", "{self.__area_name}", "{self.__requirement}")')
 
     @staticmethod
-    def __validate_salary(sallary):
+    def __validate_salary(salary):
         """
         Метод валидации salary.
         """
-        return 0 if sallary < 0 else sallary
+        if salary is None:
+            return 0
+        if isinstance(salary["from"], int) and salary["from"] > 0:
+            return salary["from"]
+        else:
+            return 0
+
+    @staticmethod
+    def __validate_data(data):
+        """
+        Метод валидации остальных полей вакансии.
+        """
+        if data:
+            return data
+        else:
+            return "Информация не указана."
 
     @classmethod
     def create_vacancy(cls, vacancy_data: dict):
         return cls(
             name=vacancy_data["name"],
-            salary_from=vacancy_data["salary_from"],
+            salary=vacancy_data["salary"],
             url=vacancy_data["url"],
-            requirement=vacancy_data["requirement]"],
+            requirement=vacancy_data["snippet"].get("requirement"),
+            area_name=vacancy_data["area"].get("name"),
         )
 
     def __gt__(self, other):
-        return self.__salary_from > other.__salary_from
+        return self.__salary > other.__salary
 
     def __lt__(self, other):
-        return self.__salary_from < other.__salary_from
+        return self.__salary < other.__salary
